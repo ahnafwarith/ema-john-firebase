19480a8de89e024c91a4b8f0510407dee338fe23
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-11 04:50:22 +0600

    used useNavigate() hook from react to redirect the user to another page when user has signed up

--- src/__steps_auth.js
+++ src/__steps_auth.js
@@ -1,10 +1,15 @@
 /* 
-1. Create a new project in console.firebase
+1. Create a new firebase project in console.firebase.google.com
 2. Install firebase with node package manager
 3. Create firebase.init.js and import getAuth to export auth
 4. Go to firebase settings > authentication|| to enable firebase auth --> email & password
 5. Create Login,signUp components setup routing and styled it according to the figma file.
 6. Attach form-field handler and form submit handler.
+7. npm install --save react-firebase-hooks
+8. useCreateUserWithEmailAndPassword()  |from react firebase hook|
+9. if user is created, redirect to another page with useNavigate() hook
+10. 
+
 */
 
 /* 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 
 const Register = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [confirmPassword, setConfirmPassword] = useState();
     const [error, setError] = useState();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
 
     const handleEmail = e => {
         setEmail(e.target.value)
@@ -27,7 +32,20 @@ const Register = () => {
             setError('Please make sure your passwords match');
             return;
         }
+        if (password.length < 6) {
+            setError('Password should have more than six characters')
+            return;
+        }
+        createUserWithEmailAndPassword(email, password)
+            .then(result => {
+                console.log(result)
+            })
     }
+
+    if (user) {
+        navigate('/')
+    }
+
 
     return (
         <div>
@@ -48,7 +66,7 @@ const Register = () => {
                             <input onBlur={handleConfirmPassword} type="password" name="confirm-password" id="" required />
                         </div>
                         <p style={{ color: 'red' }}>{error}</p>
-                        <input className='form-submit' type="submit" value="Login" />
+                        <input className='form-submit' type="submit" value="Register" />
                     </form>
                     <p>
                         Already have an account? <Link className='form-link' to='/login'>Login</Link>
