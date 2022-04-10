3bf63b5fb038b41d8d149c465520ce49e3b53cc0
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-11 05:02:20 +0600

    made login component functional with react firebase hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const navigate = useNavigate();
+
+    const handleEmailInput = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordInput = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        signInWithEmailAndPassword(email, password)
+    }
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        /* navigate('/') */
+    }
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>The Login Page</h2>
-                <form action="">
+                <form onSubmit={handleFormSubmit}>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" required />
+                        <input onBlur={handleEmailInput} type="email" name="email" id="" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" required />
+                        <input onBlur={handlePasswordInput} type="password" name="password" id="" required />
                     </div>
                     <input className='form-submit' type="submit" value="Login" />
+                    <p style={{ color: 'red' }}>{error?.message}</p>
                 </form>
                 <p>
                     New to Ema-John? <Link className='form-link' to='/register'>Create An Account</Link>
