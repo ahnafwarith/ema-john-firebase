b820ee1f9fc85265839fc035c78c6d1c8c69889f
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-11 10:25:37 +0600

    hosted

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Project Netlify link
+--> https://simple-ema-john-app.netlify.app/
 
 ## Available Scripts
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>We are secret.</h1>
-            <p>But you can know about us.</p>
+            <h1>Nibba secret.</h1>
+            <p>This is the about page</p>
         </div>
     );
 };
